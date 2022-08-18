b85ffc94d117072581bbb5c227d7f06ff5cf3a10
Amit Sahu <khushipriyanka16@gmail.com>
2022-08-18 20:35:32 +0530

    website

--- utility/utility.js
+++ utility/utility.js
@@ -2,17 +2,23 @@ let bcrypt = require("bcrypt")
 
 
 
-async function hashPassword(password){
+async function hashPassword(password) {
 
     let saltRound = await bcrypt.genSalt(10)
     let hashedPassword = await bcrypt.hash(password, saltRound)
-
+        .then(function (data) {
+            return data
+        })
+        .catch(function(err){
+            return err
+        })
     return hashedPassword
+
 }
 
 
 
-async function comparePassword(password, hashPassword){
+async function comparePassword(password, hashPassword) {
 
     let result = await bcrypt.compare(password, hashPassword)
 
